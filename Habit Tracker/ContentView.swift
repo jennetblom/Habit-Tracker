b0aa1b68c35dfca738b46ea9242b093a275a26cf
jennetblom <jeboblom20@hotmail.com>
2024-04-23 12:49:07 +0200

    created new project

---
+++ Habit Tracker/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  Habit Tracker
+//
+//  Created by Jennet on 2024-04-22.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    
+    var body: some View {
+   
+        Text("Hello")
+        
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ Habit Tracker/Habit_TrackerApp.swift
@@ -0,0 +1,17 @@
+//
+//  Habit_TrackerApp.swift
+//  Habit Tracker
+//
+//  Created by Jennet on 2024-04-22.
+//
+
+import SwiftUI
+
+@main
+struct Habit_TrackerApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
